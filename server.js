20d741426ece5907cd090df3a4d32845405fabe3
modaalaa07-lab <modaalaa07@gmail.com>
2026-01-18 14:42:21 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 3000;
    1️⃣ بيانات الربط المباشرة (بدون .env) 
    انسخ البيانات دي من Supabase وحطها هنا بالظبط
 ================================ */
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
+const SUPABASE_URL = "https://pvlmziyldmvmubhmoazd.supabase.co";
+const SUPABASE_KEY = "sb_secret_0v45QnuDfQHlhhV7VzXOIw__DsKzFPy"; // الـ Secret اللي في الصورة
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
